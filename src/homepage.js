1b63b581107896265f51e27d7b95436f5fea224d
SeasonedSoup <emmetteagoncillo1@gmail.com>
2024-12-12 19:53:53 +0800

    Design Improvement
    mainly changed the initial design of each module and added
    my footer

--- src/homepage.js
+++ src/homepage.js
@@ -5,18 +5,27 @@ const content = document.getElementById('content');
 
 export function loadHomepage() {
     const restaurant = document.createElement('div');
+    restaurant.classList.add('h1')
     restaurant.textContent = "Hell's Restaurant";
+
     
     const img = document.createElement('img');
+    img.classList.add('hellImage');
     img.src = hellImg;
     img.alt = 'hell'
     
     const desc = document.createElement('div');
+    desc.classList.add('box');
     desc.textContent = "Pain and Suffering is what your appetite just needs!";
     
     const callToAction = document.createElement('div');
+    callToAction.classList.add('box');
     callToAction.textContent = "The restaurant is well known for taking the suffering and pain within every single person stuck in this Hell Loop and use it as delicious culinary stuff like idk hot wings and carolina reaper cuisines!!! ";
 
+    const hours = document.createElement('div');
+    hours.classList.add('box');
+    
+    hours.textContent = "Hours"
     content.appendChild(restaurant);
     content.appendChild(img);
     content.appendChild(desc);
@@ -25,6 +34,7 @@ export function loadHomepage() {
 
 export function loadMenuPage() {
     const menu = document.createElement('div');
+    menu.classList.add('h1');
     menu.textContent = "Inferno Menu";
 
     const mainDish = document.createElement('div');
@@ -49,6 +59,7 @@ export function loadMenuPage() {
 
 export function loadContactPage() {
     const contact = document.createElement('div');
+    contact.classList.add('h1');
     contact.textContent = 'Contact Us!';
 
     const lucifer  = document.createElement('div');
@@ -60,6 +71,7 @@ export function loadContactPage() {
     const jared = document.createElement('div');
     jared.textContent = 'Jared'
 
+    content.appendChild(contact);
     content.appendChild(lucifer);
     content.appendChild(demon);
     content.appendChild(jared);
